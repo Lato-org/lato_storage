4b8bc52f58c318790a4ce2f94819ccea9dd6d8a3
Gregorio Galante <me@gregoriogalante.com>
2025-05-17 15:11:43 +0200

    Added controller tests

---
+++ test/controllers/lato_settings/application_controller_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+module Lato
+  class ApplicationControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      Rails.cache.clear
+      @user = lato_users(:user)
+    end
+
+    # index
+    ##
+
+    test "index should response with redirect without session" do
+      get lato_storage.root_url
+      assert_response :redirect
+    end
+
+    test "index should response with success with session" do
+      authenticate_user
+
+      get lato_storage.root_url
+      assert_response :success
+    end
+  end
+end
