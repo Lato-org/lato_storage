17116ed785dbc6e4fa32739abce38d58e9e855ad
Gregorio Galante <me@gregoriogalante.com>
2025-07-24 12:37:47 +0200

    Added PreloadDashboardDataJob to optimize loading data for dashboard

--- app/controllers/lato_storage/application_controller.rb
+++ app/controllers/lato_storage/application_controller.rb
@@ -6,17 +6,19 @@ module LatoStorage
     before_action { active_sidebar(:lato_storage); active_navbar(:lato_storage) }
 
     def index
-      @blobs_count = blobs_count
-      @attachments_count = attachments_count
-      @variant_records_count = variant_records_count
-      @deletable_blobs_count = ActiveStorage::Blob.unattached.where('active_storage_blobs.created_at < ?', 12.hours.ago).count
+      # @blobs_count = ActiveStorage::Blob.count
+      # @attachments_count = ActiveStorage::Attachment.count
+      # @variant_records_count = defined?(ActiveStorage::VariantRecord) ? ActiveStorage::VariantRecord.count : 0
+      # @deletable_blobs_count = ActiveStorage::Blob.unattached.where('active_storage_blobs.created_at < ?', 12.hours.ago).count
       
-      @total_storage = ActiveStorage::Blob.sum(:byte_size)
-      @avg_storage = @total_storage / @blobs_count if @blobs_count.positive?
+      # @total_storage = ActiveStorage::Blob.sum(:byte_size)
+      # @avg_storage = @total_storage / @blobs_count if @blobs_count.positive?
 
-      @content_types = ActiveStorage::Blob.group(:content_type).count.sort_by { |_, count| -count }.first(5)
+      # @content_types = ActiveStorage::Blob.group(:content_type).count.sort_by { |_, count| -count }.first(5)
 
-      @largest_blobs = ActiveStorage::Blob.order(byte_size: :desc).limit(3)
+      # @largest_blobs = ActiveStorage::Blob.order(byte_size: :desc).limit(3)
+      
+      @data = LatoStorage::PreloadDashboardDataJob.perform_now
     end
 
     def cleaner
@@ -40,39 +42,5 @@ module LatoStorage
 
       redirect_to lato.root_path, alert: 'You have not access to this section.'
     end
-
-    private
-
-    def blobs_count
-      return ActiveStorage::Blob.count unless LatoStorage.config.optimize_performances
-
-      if ActiveRecord::Base.connection.adapter_name.downcase == "postgresql"
-        ActiveRecord::Base.connection.execute("SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'active_storage_blobs';").first["estimate"]
-      else
-        ActiveStorage::Blob.count
-      end
-    end
-
-    def attachments_count
-      return ActiveStorage::Attachment.count unless LatoStorage.config.optimize_performances
-
-      if ActiveRecord::Base.connection.adapter_name.downcase == "postgresql"
-        ActiveRecord::Base.connection.execute("SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'active_storage_attachments';").first["estimate"]
-      else
-        ActiveStorage::Attachment.count
-      end
-    end
-
-    def variant_records_count
-      return 0 unless defined?(ActiveStorage::VariantRecord)
-      return ActiveStorage::VariantRecord.count unless LatoStorage.config.optimize_performances
-
-      if defined?(ActiveStorage::VariantRecord) && ActiveRecord::Base.connection.adapter_name.downcase == "postgresql"
-        ActiveRecord::Base.connection.execute("SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'active_storage_variant_records';").first["estimate"]
-      else
-        ActiveStorage::VariantRecord.count
-      end
-    end
-    
   end
 end
---
+++ app/jobs/lato_storage/preload_dashboard_data_job.rb
@@ -0,0 +1,63 @@
+module LatoStorage
+  class PreloadDashboardDataJob < ApplicationJob
+    SEMAPHORE_CACHE_KEY = 'LatoStorage::PreloadDashboardDataJob/semaphore'
+    DATA_CACHE_KEY = 'LatoStorage::PreloadDashboardDataJob/data'
+
+    def perform(force_execution = false)
+      return load_data unless LatoStorage.config.optimize_performances
+      
+      data = Rails.cache.read(DATA_CACHE_KEY)
+      return data || {} if Rails.cache.read(SEMAPHORE_CACHE_KEY)
+
+      if data && !force_execution
+        LatoStorage::PreloadDashboardDataJob.perform_later(true)
+        return data
+      end
+
+      Rails.cache.write(SEMAPHORE_CACHE_KEY, true, expires_in: 30.minutes)
+      data = load_data
+      Rails.cache.write(DATA_CACHE_KEY, data, expires_in: 12.hours)
+      return data
+    rescue StandardError => e
+      Rails.logger.error "Error in LatoStorage::PreloadDashboardDataJob: #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      Rails.cache.delete(SEMAPHORE_CACHE_KEY)
+      nil
+    end
+
+    private
+
+    def load_data
+      blobs_count = ActiveStorage::Blob.count
+      attachments_count = ActiveStorage::Attachment.count
+      variant_records_count = defined?(ActiveStorage::VariantRecord) ? ActiveStorage::VariantRecord.count : 0
+      deletable_blobs_count = ActiveStorage::Blob.unattached.where('active_storage_blobs.created_at < ?', 12.hours.ago).count
+      total_storage = ActiveStorage::Blob.sum(:byte_size)
+      avg_storage = blobs_count.positive? ? total_storage / blobs_count : 0
+      content_types = ActiveStorage::Blob.group(:content_type).count.sort_by { |_, count| -count }.first(5)
+      largest_blobs = ActiveStorage::Blob.order(byte_size: :desc).limit(3).map do |blob|
+        {
+          id: blob.id,
+          filename: blob.filename.to_s,
+          byte_size: blob.byte_size,
+          created_at: blob.created_at,
+          content_type: blob.content_type,
+          service_name: blob.service_name,
+          checksum: blob.checksum,
+          url: Rails.application.routes.url_helpers.rails_blob_url(blob, only_path: true, expires_in: 12.hours)
+        }
+      end
+
+      {
+        blobs_count: blobs_count,
+        attachments_count: attachments_count,
+        variant_records_count: variant_records_count,
+        deletable_blobs_count: deletable_blobs_count,
+        total_storage: total_storage,
+        avg_storage: avg_storage,
+        content_types: content_types,
+        largest_blobs: largest_blobs
+      }
+    end
+  end
+end
