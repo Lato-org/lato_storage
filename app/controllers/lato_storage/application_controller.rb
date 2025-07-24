f441e87f6bba8316b52fb66b3dd823d734f62629
Gregorio Galante <me@gregoriogalante.com>
2025-07-24 11:26:15 +0200

    Added optimize_performances option on config

--- app/controllers/lato_storage/application_controller.rb
+++ app/controllers/lato_storage/application_controller.rb
@@ -6,9 +6,9 @@ module LatoStorage
     before_action { active_sidebar(:lato_storage); active_navbar(:lato_storage) }
 
     def index
-      @blobs_count = ActiveStorage::Blob.count
-      @attachments_count = ActiveStorage::Attachment.count
-      @variant_records_count = defined?(ActiveStorage::VariantRecord) ? ActiveStorage::VariantRecord.count : 0
+      @blobs_count = blobs_count
+      @attachments_count = attachments_count
+      @variant_records_count = variant_records_count
       @deletable_blobs_count = ActiveStorage::Blob.unattached.where('active_storage_blobs.created_at < ?', 12.hours.ago).count
       
       @total_storage = ActiveStorage::Blob.sum(:byte_size)
@@ -40,5 +40,39 @@ module LatoStorage
 
       redirect_to lato.root_path, alert: 'You have not access to this section.'
     end
+
+    private
+
+    def blobs_count
+      return ActiveStorage::Blob.count unless LatoStorage.config.optimize_performances
+
+      if ActiveRecord::Base.connection.adapter_name.downcase == "postgresql"
+        ActiveRecord::Base.connection.execute("SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'active_storage_blobs';").first["estimate"]
+      else
+        ActiveStorage::Blob.count
+      end
+    end
+
+    def attachments_count
+      return ActiveStorage::Attachment.count unless LatoStorage.config.optimize_performances
+
+      if ActiveRecord::Base.connection.adapter_name.downcase == "postgresql"
+        ActiveRecord::Base.connection.execute("SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'active_storage_attachments';").first["estimate"]
+      else
+        ActiveStorage::Attachment.count
+      end
+    end
+
+    def variant_records_count
+      return 0 unless defined?(ActiveStorage::VariantRecord)
+      return ActiveStorage::VariantRecord.count unless LatoStorage.config.optimize_performances
+
+      if defined?(ActiveStorage::VariantRecord) && ActiveRecord::Base.connection.adapter_name.downcase == "postgresql"
+        ActiveRecord::Base.connection.execute("SELECT reltuples::bigint AS estimate FROM pg_class WHERE relname = 'active_storage_variant_records';").first["estimate"]
+      else
+        ActiveStorage::VariantRecord.count
+      end
+    end
+    
   end
 end
--- lib/lato_storage/config.rb
+++ lib/lato_storage/config.rb
@@ -3,8 +3,16 @@ module LatoStorage
   # This class contains the default configuration of the engine.
   ##
   class Config
-    
+
+    # This option allows to optimize performances by skipping count queries.
+    # If set to true, it will not perform count queries for collections and use other methods instead.
+    # This can significantly speed up operations but may lead to inaccuracies in counts.
+    # This is really useful for large datasets where performance is critical.
+    # Default is false.
+    attr_accessor :optimize_performances
+
     def initialize
+      @optimize_performances = false
     end
   end
 end
